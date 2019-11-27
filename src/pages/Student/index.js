e1d7c7a3e286f74b06b1e402f1d0998ee943a653
Rafael Meira <rafael.meira@althaia.com.br>
2019-11-27 10:37:15 -0300

    update aluno

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import CardActivy from '../../components/CardActivy';
 
 export default function Student() {
-	const [ user, setUser ] = useState([]);
+	const [user, setUser] = useState([]);
 
 	useEffect(() => {
 		const user = localStorage.getItem('user');
@@ -12,8 +12,11 @@ export default function Student() {
 	return (
 		<div className='student'>
 			<div className='studentContainer'>
-				<h1>Minhas Atividades</h1>
+				<span>Minhas Atividades</span>
 				{user.activy && user.activy.map((activy) => <CardActivy user={activy} />)}
+			</div>
+			<div className='studentMaterial'>
+				
 			</div>
 		</div>
 	);
