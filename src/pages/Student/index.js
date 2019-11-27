c15bbc6c9913b929ff4941f79fdf4571492503fb
Victor Rodrigues <victorrodrigues@MacBook-Pro-de-Victor.local>
2019-11-27 11:28:33 -0300

    Finalizando card alunos

--- src/pages/Student/index.js
+++ src/pages/Student/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import CardActivy from '../../components/CardActivy';
+import videos from './videoRelated';
 
 export default function Student({ user }) {
 	return (
 		<div className='student'>
 			<div className='studentContainer'>
-				<span>Minhas Atividades</span>
-				{user.activy && user.activy.map((activy) => <CardActivy user={activy} />)}
+				<div>
+					<span>Minhas Atividades</span>
+					{user.activy && user.activy.map((activy) => <CardActivy user={activy} />)}
+				</div>
+
+				<div className='studentMaterial'>
+					<h1>Materiais complementares</h1>
+					{videos.map((video) => <img src={video.img} alt='' />)}
+				</div>
 			</div>
-			<div className='studentMaterial' />
 		</div>
 	);
 }
---
+++ src/pages/Student/videoRelated.js
@@ -0,0 +1,16 @@
+import ImageVideo from '../../assets/Rectangle 70.svg';
+import ImageVideo2 from '../../assets/video2.png';
+
+const video = [
+	{
+		img: ImageVideo
+	},
+	{
+		img: ImageVideo2
+	},
+	{
+		img: ImageVideo
+	}
+];
+
+export default video;
